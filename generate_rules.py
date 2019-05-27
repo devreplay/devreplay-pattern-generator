7f7233694a81870079f249f79417d009a297a2f4
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2019-05-27 18:34:15 +0900

    fix threshold

--- generate_rules.py
+++ generate_rules.py
@@ -10,10 +10,9 @@ owner = config["Target"]["owner"]
 repo = config["Target"]["repo"]
 lang = config["Target"]["lang"]
 rule_method = config["Rule"]["frequent_or_topk"]
-threshold = int(config["Rule"]["threshold"])
+thresholds = [int(x) for x in config["Rule"]["thresholds"].split()]
 
 INPUT_JSON_NAME = "data/changes/" + owner + "_" + repo + "_" + lang + ".json"
-OUTPUT_JSON_NAME = "data/rules/" + owner + "_" + repo + "_" + lang + ".json"
 
 def remove_redundant_symbols(code):
     tokens = []
@@ -44,18 +43,29 @@ def generate_rules(changes_sets, threshold):
     ps = PrefixSpan(changes_sets)
     print("Start rule generation")
     # freq_seqs = ps.frequent(minsup=int(len(new_changes) * 0.1), closed=True)
-    if rule_method == "frequent":
-        freq_seqs = PrefixSpan_frequent(ps, minsup=threshold, closed=True)
-    elif rule_method == "topk":
-        freq_seqs = PrefixSpan_topk(ps, k=threshold, closed=True)
-    else:
-        freq_seqs = PrefixSpan_frequent(ps, minsup=threshold, closed=True)
-    
-    freq_seqs = [x for x in freq_seqs
-                 if any([y.startswith("+") for y in x[1]]) and
-                 any([y.startswith("-") for y in x[1]])
-                 ]
+    rule_len = 0
+    while rule_len == 0 or threshold!=0:
+        if rule_method == "frequent":
+            freq_seqs = PrefixSpan_frequent(ps, minsup=threshold, closed=True)
+        elif rule_method == "topk":
+            freq_seqs = PrefixSpan_topk(ps, k=threshold, closed=True)
+        else:
+            freq_seqs = PrefixSpan_frequent(ps, minsup=threshold, closed=True)
+        
+        freq_seqs = [x for x in freq_seqs
+                    if any([y.startswith("+") for y in x[1]]) and
+                    any([y.startswith("-") for y in x[1]])
+                    ]
+        rule_len = len(freq_seqs)
+        if rule_method == "frequent":
+            print("Fix threshold " + str(threshold) + " to " + str(threshold / 2))
+            threshold /= 2
+        elif rule_method == "topk":
+            print("Fix threshold " + str(threshold) + " to " + str(threshold * 2))
+            threshold *= 2
 
+
+    print("Final threshold is " + str(threshold))
     freq_seqs = sorted(freq_seqs, reverse=True)
     return freq_seqs
 
@@ -75,16 +85,19 @@ changes = [[x for x in tokens
             if not x.endswith("\n") and not x.endswith(" ")]
            for tokens in changes]
 
-freq_seqs = generate_rules(changes, threshold)
+for threshold in thresholds:
+    OUTPUT_JSON_NAME = "data/rules/" + owner + "_" + repo + "_"  + str(threshold) + "_" + lang + ".json"
+    freq_seqs = generate_rules(changes, threshold)
 
-new_rules = []
+    new_rules = []
 
-for i, rule in enumerate(freq_seqs):
-    count = rule[0]
-    code = rule[1]
-    trigger_tokens = reduce(lambda x,y :x+y ,[x[2:].split(" ") if " " in x[2:] else [x[2:]] for x in code if not x.startswith("+")])
-    code = remove_redundant_symbols(code)
-    new_rules.append({"count": count, "code": code, "trigger": trigger_tokens})
+    for i, rule in enumerate(freq_seqs):
+        count = rule[0]
+        code = rule[1]
+        trigger_tokens = reduce(lambda x,y :x+y ,[x[2:].split(" ") 
+        if " " in x[2:] else [x[2:]] for x in code if not x.startswith("+")])
+        code = remove_redundant_symbols(code)
+        new_rules.append({"count": count, "code": code, "trigger": trigger_tokens})
 
-with open(OUTPUT_JSON_NAME, mode='w', encoding='utf-8') as f:
-    dump(new_rules, f, indent=1)
+    with open(OUTPUT_JSON_NAME, mode='w', encoding='utf-8') as f:
+        dump(new_rules, f, indent=1)
