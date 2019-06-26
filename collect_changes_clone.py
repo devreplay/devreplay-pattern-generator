aba1777748bd8c274bead9b25ffb3e00af1a6e59
wata727 <watassbass@gmail.com>
2019-06-26 15:35:51 +0900

    Make private repository aware

--- collect_changes_clone.py
+++ collect_changes_clone.py
@@ -61,7 +61,7 @@ def clone_target_repo():
     if not os.path.exists(data_repo_dir):
         os.makedirs(data_repo_dir)
     if not os.path.exists(data_repo_dir + "/" + repo):
-        git.Git(data_repo_dir).clone("https://github.com/" + owner + "/" + repo +".git")
+        git.Git(data_repo_dir).clone("https://" + config["GitHub"]["Token"] + ":@github.com/" + owner + "/" + repo +".git")
 
 def make_pull_diff(target_repo, diff_path):
     change_sets = []
