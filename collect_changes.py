a79129796338f39d6ffdbf9584e0271fa6744c02
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2019-08-19 16:33:30 +0900

    combine collect changes

--- collect_changes.py
+++ collect_changes.py
@@ -25,7 +25,10 @@ config.read('config')
 owner = config["Target"]["owner"]
 repo = config["Target"]["repo"]
 lang = config["Target"]["lang"]
-change_size = int(config["Target"]["change_size"])
+change_size = int(config["Option"]["change_size"])
+learn_from = config["Option"]["learn_from"]
+
+
 TN = TokeNizer(lang)
 DIVIDE_PER = 100
 
@@ -35,12 +38,40 @@ def main():
     """
     clone_target_repo()
     target_repo = git.Repo("data/repos/" + repo)
-    changes_sets = make_master_diff(target_repo, lang)
 
-    out_name = "data/changes/" + owner + "_" + repo + "_" + lang + "ss.json"
+    if learn_from == "pulls":
+        out_name = "data/changes/" + owner + "_" + repo + "_" + lang + "_pulls.json"
+        changes_sets = get_project_changes(owner, repo, lang, target_repo)
+    else:
+        if learn_from != "master":
+            print("Collect from master changes...")
+        out_name = "data/changes/" + owner + "_" + repo + "_" + lang + "_master.json"
+        changes_sets = make_master_diff(target_repo, lang)
+
 
     with open(out_name, "w", encoding='utf-8') as f:
         dump(changes_sets, f, indent=1)
+
+
+def get_project_changes(owner, repo, lang, target_repo, diffs_file=None):
+    changes_sets = []
+    diffs_file = "data/pulls/" + owner + "_" + repo + ".csv"
+    with open(diffs_file, "r", encoding="utf-8") as diffs:
+        reader = DictReader(diffs)
+        for i, diff_path in enumerate(reversed(list(reader))):
+            if diff_path["commit_len"] == "1":
+                continue
+            sys.stdout.write("\r%d pulls id: %s, %d / %d changes" % 
+                             (i, diff_path["number"], len(changes_sets), change_size))
+
+            changes_set = make_pull_diff(target_repo, diff_path)
+            if changes_set == []:
+                continue
+            changes_sets.extend(changes_set)
+            if len(changes_sets) > change_size:
+                return changes_sets
+
+    return changes_sets
 
 
 def clone_target_repo():
@@ -79,6 +110,9 @@ def make_hunks(source, target):
             deleted_lines.append(line)
         elif symbol == "+":
             added_lines.append(line)
+        else:
+            deleted_lines = []
+            added_lines = []
 
         previous_symbol = symbol
     if deleted_lines != [] and added_lines != []:
@@ -89,7 +123,8 @@ def make_hunks(source, target):
     return hunks
 
 def is_valued_change(diff):
-    return diff["identifiers"]["condition"] != diff["identifiers"]["consequent"]
+    # return diff["identifiers"]["condition"] != diff["identifiers"]["consequent"] and\
+    return diff["identifiers"]["condition"] != [] and diff["identifiers"]["consequent"] != [] 
 
 def make_master_diff(target_repo, lang):
     change_sets = []
@@ -115,14 +150,6 @@ def make_master_diff(target_repo, lang):
             hunks = make_hunks(source.splitlines(keepends=True), target.splitlines(keepends=True))
 
             for hunk in hunks:
-                # try:
-                #     diff_result = TN.get_abstract_tree_diff(hunk["source"], hunk["target"])
-                # except:
-                #     continue
-                # if not is_valued_change(diff_result):
-                #     continue
-                # hunk["source"] = tokens2Realcode(diff_result["condition"])
-                # hunk["target"] = tokens2Realcode(diff_result["consequent"])
 
                 out_metricses = {
                     "sha": sha,
@@ -138,6 +165,52 @@ def make_master_diff(target_repo, lang):
         if i > change_size:
             break
         
+    return change_sets
+
+def make_pull_diff(target_repo, diff_path):
+    change_sets = []
+    try :
+        original_commit = target_repo.commit(diff_path["first_commit_sha"])
+        changed_commit = target_repo.commit(diff_path["merge_commit_sha"])
+    except:
+        return []
+    commits = target_repo.iter_commits(diff_path["first_commit_sha"] + ".." + diff_path["merge_commit_sha"])
+    if any([x.message.startswith("Merge") for x in commits]):
+        return []
+    diff_index = original_commit.diff(changed_commit)    
+    for diff_item in [x for x in diff_index.iter_change_type('M')
+                     if any([x.a_rawpath.decode('utf-8').endswith(y)
+                             for y in lang_extentions[lang]])]:
+        source = diff_item.a_blob.data_stream.read().decode('utf-8')
+        target = diff_item.b_blob.data_stream.read().decode('utf-8')
+        if source == target:
+            continue
+        hunks = make_hunks(source.splitlines(keepends=True), target.splitlines(keepends=True))   
+
+        for hunk in hunks:
+            try:
+                diff_result = TN.get_abstract_tree_diff(hunk["source"], hunk["target"])
+            except:
+                continue
+
+            if not is_valued_change(diff_result):
+                continue
+
+            out_metricses = {
+                "number": int(diff_path["number"]),
+                "sha": diff_path["merge_commit_sha"],
+                "author":diff_path["author"],
+                "participant":diff_path["participant"],
+                "created_at": diff_path["created_at"],
+                "merged_at": diff_path["merged_at"],
+                "merged_by": diff_path["merged_by"],
+                "file_path": diff_item.a_rawpath.decode('utf-8'),
+                "condition": tokens2Realcode(diff_result["condition"]).splitlines(),
+                "consequent": tokens2Realcode(diff_result["consequent"]).splitlines()
+            }
+            if out_metricses["condition"] != []:
+                change_sets.append(out_metricses)
+
     return change_sets
 
 if __name__ == '__main__':
