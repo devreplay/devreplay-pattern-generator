7203efcaba4c83437701c00279dd13bbf2f0faee
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2019-08-28 15:32:59 +0900

    add .c as CPP

--- lang_extentions.py
+++ lang_extentions.py
@@ -1,6 +1,6 @@
 lang_extentions = {
     "Java": [".java", ".jav"],
-    "CPP": [".cpp", ".cc", ".cxx", ".hpp", ".hh", ".hxx", ".h", ".ino", ".inl", ".ipp"],
+    "CPP": [".cpp", ".cc", ".cxx", ".hpp", ".hh", ".hxx", ".h", ".ino", ".inl", ".ipp", ".c"],
     "Python": [".py", ".rpy", ".pyw", ".cpy", ".gyp", ".gypi", ".snakefile", ".smk", ".pyi", ".ipy"],
     "JavaScript": [".js", ".es6", ".mjs", ".pac", ".ts"],
     "Ruby": [".arb",".builder",".cgi",".fcgi",".gemspec",".god",".irbrc",".jbuilder",".mspec",".pluginspec",".podspec",".prawn",".pryrc",".rabl",".rake",".rb",".rbuild",".rbw",".rbx",".rjs",".ru",".ruby",".spec",".thor",".watchr"]
