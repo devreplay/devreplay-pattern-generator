e5340cf9fa84c96a76d9cc7112f14592011d1a4d
kenjiro <kenjiro@isb3-dhcp-172-235.naist.jp>
2019-09-12 10:19:37 +0900

    fix make_rules path

--- make_rules.sh
+++ make_rules.sh
@@ -1,2 +1,2 @@
-python3 /Users/kenjiro/Documents/Ueda/review_pattern_gen/collect_changes.py
-python3 /Users/kenjiro/Documents/Ueda/review_pattern_gen/test_rules.py
+python3 collect_changes.py
+python3 test_rules.py
