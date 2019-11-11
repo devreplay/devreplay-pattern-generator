58f068ccf436b800289ced26dbda0eb204e22386
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2019-11-11 13:48:21 +0900

    fix js parser

--- lang_extentions.py
+++ lang_extentions.py
@@ -2,7 +2,7 @@ lang_extentions = {
     "Java": [".java", ".jav"],
     "CPP": [".cpp", ".cc", ".cxx", ".hpp", ".hh", ".hxx", ".h", ".ino", ".inl", ".ipp", ".c"],
     "Python": [".py", ".rpy", ".pyw", ".cpy", ".gyp", ".gypi", ".snakefile", ".smk", ".pyi", ".ipy"],
-    "JavaScript": [".js", ".es6", ".mjs", ".pac", ".ts"],
+    "JavaScript": [".js", ".es6", ".mjs", ".pac"],
     "Ruby": [".arb",".builder",".cgi",".fcgi",".gemspec",".god",".irbrc",".jbuilder",".mspec",".pluginspec",".podspec",".prawn",".pryrc",".rabl",".rake",".rb",".rbuild",".rbw",".rbx",".rjs",".ru",".ruby",".spec",".thor",".watchr"],
     "PHP": [".php", ".php4", ".php5", ".phtml", ".ctp"]
 }
