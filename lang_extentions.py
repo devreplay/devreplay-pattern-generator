916abfe36d010f3adaa0de26afa7336e61d150d7
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2020-01-07 17:21:23 +0900

    add Dart and Go

--- lang_extentions.py
+++ lang_extentions.py
@@ -4,5 +4,7 @@ lang_extentions = {
     "Python": [".py", ".rpy", ".pyw", ".cpy", ".gyp", ".gypi", ".snakefile", ".smk", ".pyi", ".ipy"],
     "JavaScript": [".js", ".es6", ".mjs", ".pac", "ts"],
     "Ruby": [".arb",".builder",".cgi",".fcgi",".gemspec",".god",".irbrc",".jbuilder",".mspec",".pluginspec",".podspec",".prawn",".pryrc",".rabl",".rake",".rb",".rbuild",".rbw",".rbx",".rjs",".ru",".ruby",".spec",".thor",".watchr"],
-    "PHP": [".php", ".php4", ".php5", ".phtml", ".ctp"]
+    "PHP": [".php", ".php4", ".php5", ".phtml", ".ctp"],
+    "Go": [".go"],
+    "Dart": [".dart"]
 }
