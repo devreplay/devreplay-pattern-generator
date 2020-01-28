1ce4c769f2111e7bbcaa0adefa08df3f2fe25b97
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2020-01-28 11:13:27 +0900

    fix test_rules

--- test_rules.py
+++ test_rules.py
@@ -7,6 +7,8 @@ from pathlib import Path
 from lang_extentions import lang_extentions
 from collections import defaultdict
 from datetime import datetime as dt
+from devreplay_simulate_util import snippet2RegexCondition
+from datetime import timedelta
 
 with open("config.json", "r") as json_file:
     config = json.load(json_file)
@@ -24,38 +26,16 @@ else:
 
 repos = [x["repo"] for x in projects]
 
-validate_projects = config.get("applied_projects", [])
-
 learn_from = "pulls" if "pull" in config["learn_from"] else "master"
-
-group_changes = re.compile(r"\\\$\\{(\d+):[a-zA-Z_]+\\}")
 
 change_files = ["data/changes/" + x["owner"] + "_" + x["repo"] + "_" + lang  + "_"  + learn_from + ".json"
             for x in projects]
 changes = []
 for change_file in change_files:
-    print("Combine from " + change_file)
+    print("Combine changes from " + change_file)
     with open(change_file, "r") as target:
         data = json.load(target)
         changes.extend(data)
-
-def group2increment(matchobj, identifier_ids):
-    tokenid = int(matchobj.group(1))
-    if tokenid in identifier_ids:
-        return r"(?P=token" + str(tokenid + 1) + r")"
-    else:
-        identifier_ids.append(tokenid)
-        return r"(?P<token" + str(tokenid + 1) + r">[\w\s_]+)"
-
-def snippet2Regex(snippet):
-    identifier_ids = []
-    joinedCondition = "\n".join(snippet)
-    joinedCondition = re.escape(joinedCondition)
-    joinedCondition = group_changes.sub(lambda m: group2increment(m, identifier_ids), joinedCondition)
-    try:
-        return re.compile(joinedCondition)
-    except:
-        return
 
 def clone_target_repo(owner, repo):
     data_repo_dir = "data/repos"
@@ -84,8 +64,10 @@ def get_all_file_contents(repo):
     return {f"{repo}/{x}": target_repo.git.show('HEAD:{}'.format(x)) for x in paths}
 
 def make_matched_files(contents, re_condition, re_consequent):
-    condition_files = {path for path, content in contents.items() if re_condition.search(content)}
     consequent_files = {path for path, content in contents.items() if re_consequent.search(content)}
+    if not consequent_files:
+        return ({}, {})
+    condition_files = {path for path, content in contents.items() if re_condition.search(content)}
 
     # origin_condition = condition_files.difference(consequent_files)
     return (condition_files, consequent_files.difference(condition_files))
@@ -100,56 +82,32 @@ for project in projects:
     all_contents.update(file_contents)
 print("Success Collecting %d files!" % len(all_contents))
 
-all_validate_contents = {}
-if validate_projects != []:
-    print("Collecting validate file contents...")
-    for project in validate_projects:
-        print(project)
-        clone_target_repo(project["owner"], project["repo"])
-        file_contents = get_all_file_contents(project["repo"])
-        all_validate_contents.update(file_contents)
-
-    print("Success Collecting %d files!" % len(all_validate_contents))
 
 print("Checking Rules...")
 
 duplicates_sha = []
 
-changes_size = len(changes)
 all_changes = []
+
+changes_size = len(changes)
 
 for i, change in enumerate(changes):
     if (change["condition"], change["consequent"]) in duplicates_sha:
         continue
 
-    re_condition = snippet2Regex(change["condition"])
-    re_consequent = snippet2Regex(change["consequent"])
+    re_condition = snippet2RegexCondition(change["condition"])
+    re_consequent = snippet2RegexCondition(change["consequent"])
     if re_condition == None or re_consequent == None:
         continue
-    sys.stdout.write("\r%d / %d changes, %d rules collected" % (i, changes_size, len(all_changes)))
+    sys.stdout.write("\r%d / %d changes, %d rules are collected" % (i, changes_size, len(all_changes)))
 
     origin_condition, origin_consequent = make_matched_files(all_contents, re_condition, re_consequent)
-    condition_len = len(origin_condition)
     consequent_len = len(origin_consequent)
+    if consequent_len == 0 or len(origin_condition) == 0:
+        continue
 
-    change["popularity"] = consequent_len / len(origin_condition.union(origin_consequent))\
-                           if consequent_len > 0 else 0
-
-    if len(projects) > 1:
-        condition_project = {x for x in repos if any([y.startswith(x) for y in origin_condition])}
-        consequent_project = {x for x in repos if any([y.startswith(x) for y in origin_consequent])}
-        change["projects_popularity"] = len(consequent_project.difference(condition_project)) /\
-                                        len(condition_project.union(consequent_project))\
-                                        if len(consequent_project) > 0 else 0
-        change["applied_projects"] = list(consequent_project)
-
-    if validate_projects != []:
-        origin_condition, validate_consequent = make_matched_files(all_validate_contents, re_condition, re_consequent)
-        condition_len = len(origin_condition)
-        validate_consequent_len = len(validate_consequent)
-        change["self_popularity"] = validate_consequent_len / len(origin_condition.union(validate_consequent))\
-               if validate_consequent_len > 0 else 0
-        change["applicable_files"] = list(origin_condition)
+    change["popularity"] = consequent_len / len(origin_condition.union(origin_consequent))
+    change["applicable_files"] = list(origin_condition)
     
     if change["popularity"] > 0.01:
         change["links"] = ["https://github.com/%s/commit/%s" % (change["repository"], change["sha"])]
@@ -161,7 +119,7 @@ validate_by = "pulls" if "pull" in config["validate_by"] else "master"
 
 if validate_by != learn_from:
     changes = []
-    change_files = ["data/changes/" + x["owner"] + "_" + x["repo"] + "_" + lang  + "_"  + validate_by + ".json"
+    change_files = ["data/changes/" + x["owner"] + "_" + x["repo"] + "_" + lang  + "_"  + validate_by + "2.json"
             for x in projects]
     for change_file in change_files:
         print("Combine from " + change_file)
@@ -169,49 +127,49 @@ if validate_by != learn_from:
             data = json.load(target)
             changes.extend(data)
 
-contents = "number" if "pulls" == validate_by else "sha"
-contents_type = "pull" if "pulls" == validate_by else "commit"
-successed_numbers = []
-changes_size = len(all_changes)
-output = []
+# contents = "number" if "pulls" == validate_by else "sha"
+# contents_type = "pull" if "pulls" == validate_by else "commit"
+# successed_numbers = []
+# changes_size = len(all_changes)
+# output = []
 
-for i, change in enumerate(all_changes):
-    if "/".join([change["repository"], contents_type, str(change[contents])]) in successed_numbers:
-        continue
-    sys.stdout.write("\r%d / %d rules are collected" %
-                    (i + 1, changes_size))
-    tmp_change = change
+# for i, change in enumerate(all_changes):
+#     if "/".join([change["repository"], contents_type, str(change[contents])]) in successed_numbers:
+#         continue
+#     sys.stdout.write("\r%d / %d rules are collected" %
+#                     (i + 1, changes_size))
+#     tmp_change = change
 
-    re_condition = snippet2Regex(change["condition"])
-    re_consequent = snippet2Regex(change["consequent"])
+#     re_condition = snippet2RegexCondition(change["condition"])
+#     re_consequent = snippet2RegexCondition(change["consequent"])
 
-    date = dt.strptime(change["created_at"],"%Y-%m-%d %H:%M:%S")
-    condition_change = [x for x in changes
-                        if re_condition.search("\n".join(x["condition"]))
-                        and dt.strptime(x["created_at"],"%Y-%m-%d %H:%M:%S") > date]
+#     date = dt.strptime(change["created_at"],"%Y-%m-%d %H:%M:%S")
+#     condition_change = [x for x in changes
+#                         if re_condition.search("\n".join(x["condition"]))
+#                         and dt.strptime(x["created_at"],"%Y-%m-%d %H:%M:%S") > date]
 
-    if len(condition_change) == 0:
-        output.append(tmp_change)
-        continue
-    consequent_change = ["/".join([x["repository"], contents_type, str(x[contents])]) for x in condition_change if re_consequent.search("\n".join(x["consequent"]))]
+#     if len(condition_change) == 0:
+#         output.append(tmp_change)
+#         continue
+#     consequent_change = ["/".join([x["repository"], contents_type, str(x[contents])]) for x in condition_change if re_consequent.search("\n".join(x["consequent"]))]
 
-    successed_number = list({x for x in consequent_change if x not in successed_numbers})
-    tmp_change["exception_links"] = list({"https://github.com/%s/%s/%s" % (x["repository"], contents_type, str(x[contents])) for x in condition_change
-                                            if "/".join([x["repository"], contents_type, str(x[contents])]) not in successed_number})
+#     successed_number = list({x for x in consequent_change if x not in successed_numbers})
+#     tmp_change["exception_links"] = list({"https://github.com/%s/%s/%s" % (x["repository"], contents_type, str(x[contents])) for x in condition_change
+#                                             if "/".join([x["repository"], contents_type, str(x[contents])]) not in successed_number})
 
-    successed_numbers.extend(successed_number)
-    tmp_change["links"].extend(["https://github.com/%s" % x for x in successed_number])
-    tmp_change["frequency"] = len(successed_number)
-    tmp_change["accuracy"] = tmp_change["frequency"] / (tmp_change["frequency"] + len(tmp_change["exception_links"]))
-    output.append(tmp_change)
+#     successed_numbers.extend(successed_number)
+#     tmp_change["links"].extend(["https://github.com/%s" % x for x in successed_number])
+#     tmp_change["frequency"] = len(successed_number)
+#     tmp_change["accuracy"] = tmp_change["frequency"] / (tmp_change["frequency"] + len(tmp_change["exception_links"]))
+#     output.append(tmp_change)
 
 if len(projects) == 1:
     OUT_TOKEN_NAME = "data/changes/" + projects[0]["owner"] + "_" + projects[0]["repo"] + \
-    "_" + lang + "_" + learn_from + "_devreplay.json"
+    "_" + lang + "_" + learn_from + "_devreplay2.json"
 else:
     OUT_TOKEN_NAME = "data/changes/devreplay.json"
 
-output = sorted(output, key=lambda x: x["popularity"], reverse=True)
+output = sorted(all_changes, key=lambda x: x["popularity"], reverse=True)
 with open(OUT_TOKEN_NAME, "w") as target:
     print("Success to validate the changes Output is " + OUT_TOKEN_NAME)
     json.dump(output, target, indent=2)
